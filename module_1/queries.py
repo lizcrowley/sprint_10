0a91a6b991f6edbc39cc39fc348bbfb5dfbe65f9
lizcrowley <lizcrowley12@gmail.com>
2023-10-23 22:56:22 -0500

    adds a bigger query and pulls results into a csv file

--- module_1/queries.py
+++ module_1/queries.py
@@ -1,3 +1,12 @@
 '''docstring'''
 SELECT_ALL = "SELECT character_id, name FROM charactercreator_character;"
 
+AVG_ITEM_WEIGHT_PER_CHARACTER = '''
+SELECT cc_char.name, ai.weight 
+FROM charactercreator_character AS cc_char
+JOIN charactercreator_character_inventory AS cc_inv
+ON cc_char.character_id = cc_inv.character_id
+JOIN armory_item AS ai
+ON ai.item_id = cc_inv.item_id
+GROUP BY cc_char.character_id
+'''
--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,24 +1,32 @@
 # Step 0 - import sqlite3
+'''Must import'''
 import sqlite3
 import queries as q
-
-# Step 1
-# Connect to the database
-# triple-check the spelling of your database filename, else you may create a new database
-connection = sqlite3.connect('rpg_db.sqlite3')
+import pandas as pd
 
 
-# Step 2 - Make the "cursor"
-cursor = connection.cursor()
+# DB Connect function
+def connect_to_db(db_name='rpg_db.sqlite3'):
+    '''Establishes connecton to database'''
+    return sqlite3.connect(db_name)
 
 
-# Step 3 - Write a query
-# See the queries.py file
+def execute_q(conn, query):
+    '''Does everything described below'''
+    # Make the cursor
+    curs = conn.cursor()
+    # Execute the query
+    curs.execute(query)
+    # Pull (and return) the results
+    return curs.fetchall()
 
-
-# Step 4 - Execute the query on the cursor and fetch the results
-# "pulling the results" from the cursor
-results = cursor.execute(q.SELECT_ALL).fetchall()
 
 if __name__ == "__main__":
-    print(results[:5])
+    conn = connect_to_db()
+    # print(execute_q(conn, q.SELECT_ALL)[:5])
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER)[:5]
+    df = pd.DataFrame(results)
+    df.columns = ['name', 'average_item_weight']
+
+    # To make a csv
+    df.to_csv('rpg_db.csv', index=False)
